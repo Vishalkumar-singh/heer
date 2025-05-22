8745802f11ed9e482a5d3c7720282ff96b26e383
vishal kumar singh <kumarsinghvishal496@gmail.com>
2025-05-22 15:56:13 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 const key = "123";
-const key2 = "555";
+const key2 = "MERIHEER";
 let checkPass = document.querySelector(".inPass");
 let pass = document.querySelector(".yourPass");
 let loginP = document.querySelector(".login");
